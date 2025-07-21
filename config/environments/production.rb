171b478e51a4d36119efc524c541b1319383ca97
Ethan.Fitzsimmons <maythefitzbewithyou34@gmail.com>
2025-07-21 18:43:48 +0000

    config.action_controller.forgery_protection_origin_check = false for testing only on ubuntu server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,11 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  
+  # For dev testing only
+  config.action_controller.forgery_protection_origin_check = false
+
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
