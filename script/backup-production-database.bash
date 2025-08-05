44aebe291a3cb7f2675b92c353d3e646887838c7
Tom Webster <tom@samurailink3.com>
2025-08-05 20:14:01 +0000

    Script: Update name of prod DB container

--- script/backup-production-database.bash
+++ script/backup-production-database.bash
@@ -2,4 +2,4 @@
 
 # ref: https://stackoverflow.com/a/29913462
 TIMESTAMP=$(date --utc --iso-8601=seconds | tr -d :)
-podman exec -t sta-STA-DB-1 pg_dumpall -c -U sta-production-admin | gzip > ./storage/backups/prod-db_$TIMESTAMP.sql.gz
+podman exec -t sta_STA-DB_1 pg_dumpall -c -U sta-production-admin | gzip > ./storage/backups/prod-db_$TIMESTAMP.sql.gz
